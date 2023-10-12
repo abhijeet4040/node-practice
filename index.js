1931c87ddb18b6a86f3b97042d6503e9008d6614
abhijeet <abhijeet.k@binmile.com>
2023-10-12 17:58:24 +0530

    .

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const multer = require('multer');
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const fs = require("fs");
+const { timeStamp } = require("console");
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const upload = multer({ dest: "./images" });
+// const upload = multer({ dest: "./images" });
 
-app.post("/", upload.single("file"), (req, res) => {
-   const name = req.file.encoding
-   console.log(name)
-    console.log(req.file);
+app.post("/", multer().single("file"), (req, res) => {
+  console.log(req.file);
+
+  const timeStamp = Date.now();
+  // const newFileName = `${timestamp}_${req.file.originalname}`
+  const fileName = req.file.originalname
+  const newPath = `./images/${timeStamp}${fileName}`;
+  fs.writeFileSync(newPath, req.file.buffer)
 });
 
 const port = process.env.PORT || 3000;
