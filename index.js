7a1c251db6e8029e543e9605a42c7631d268bee1
abhijeet <abhijeet.k@binmile.com>
2023-10-16 15:13:54 +0530

    .

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3000;
 const crypto = require('crypto');
 
-const algorithm = 'aes-256-cbc';
-const key = 'anything';
-const iv = crypto.randomBytes(16);
+// const algorithm = 'aes-256-cbc';
+// const key = 'anything';
+// const iv = crypto.randomBytes(16);
 
-const cipher = crypto.createCipheriv(algorithm, key, iv);
+// const cipher = crypto.createCipheriv(algorithm, key, iv);
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
