392702420a9b183253d6c5372809d28c2f1ee510
abhijeet <abhijeet.k@binmile.com>
2023-10-16 15:03:52 +0530

    .

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const crypto = require('crypto');
+
+const algorithm = 'aes-256-cbc';
+const key = 'anything';
+const iv = crypto.randomBytes(16);
+
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+ app.listen(port, ()=> {
+    console.log('server is running on port ' + port);
+ } )
+
+
+
