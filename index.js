de5bba674caf8db4ffd4809636532914c3b1e3d1
abhijeet <abhijeet.k@binmile.com>
2023-10-17 15:47:22 +0530

    .

--- index.js
+++ index.js
@@ -2,19 +2,47 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const crypto = require('crypto');
+const { json } = require('body-parser');
 
-// const algorithm = 'aes-256-cbc';
-// const key = 'anything';
-// const iv = crypto.randomBytes(16);
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
 
-// const cipher = crypto.createCipheriv(algorithm, key, iv);
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
+app.use(express.json());
+
+app.post('/cipher-password', (req, res) => {
+    const { text } = req.body;
+    
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+    let encryptedPassword = cipher.update(text, 'utf-8', 'hex')  
+
+    encryptedPassword += cipher.final('hex') 
+    console.log(encryptedPassword)
+    res.json({ "text" : encryptedPassword,
+"key":key,
+"iv":iv 
+})
+
+ 
 });
- app.listen(port, ()=> {
-    console.log('server is running on port ' + port);
- } )
 
+app.post('/decipher-password', (req, res) => {
+    let { text,key,iv } = req.body;
+    key=Buffer.from(key,'hex')
+    iv=Buffer.from(iv,'hex')
+    const decipher = crypto.createDecipheriv(algorithm, key, iv)
+    
+    let decryptedPassword = decipher.update(text, 'hex', 'utf-8')
+    
+    decryptedPassword += decipher.final('utf-8');
+    console.log(decryptedPassword)
+    res.send({text: decryptedPassword})
+})
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port);
+}); 
 
 
