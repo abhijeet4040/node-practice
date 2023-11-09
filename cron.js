cd7320c537cb77df6389fc0e3eef17ff4b347fee
abhijeet <abhijeet.k@binmile.com>
2023-11-09 12:11:32 +0530

    .

---
+++ cron.js
@@ -0,0 +1,8 @@
+// const cron = require('node-cron');
+// const user = require('./models/user');
+
+// cron.schedule('*/5 * * * *', async () => {
+//   const result = await  user.findAll();
+//   console.log(result);
+// });
+
--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const app = express();
 const port = 3000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const role = require('./models/role');
-const user = require('./models/user');
 global.app = app;
 app.use(cors())
 app.use(bodyParser.json())
@@ -15,6 +13,7 @@ require('./models/user')
 require('./models/role')
 require('./routes/user.routes')
 require('./routes/role.routes')
+require('./cron')
 
 app.listen(port, () => {
     console.log('listening on port ' + port)
